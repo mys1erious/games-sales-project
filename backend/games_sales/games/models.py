9b0a76ff50434cd6be21137be55e627350dd3b06
mys1erious <subtletyzeros@gmail.com>
2022-04-13 00:22:00 +0200

    Added blank and null attributes to Game Model

--- backend/games_sales/games/models.py
+++ backend/games_sales/games/models.py
@@ -1,20 +1,7 @@
 from django.db import models
 
 
-class Game(models.Model):
-    name = models.CharField(max_length=120, unique=True)
-    platform = models.CharField(max_length=30, )
-    year_of_release = models.IntegerField()
-    genre = models.CharField(max_length=30)
-    publisher = models.CharField(max_length=30)
-    developer = models.CharField(max_length=30)
-    esrb_rating = models.CharField(max_length=1)
-    game_rating = Rating()
-    sale_info = SaleInfo()
-
-
 class Rating(models.Model):
-    game = ...  # Game Model ?
     critic_score = models.FloatField()
     critic_count = models.FloatField()
     user_score = models.FloatField()
@@ -22,9 +9,21 @@ class Rating(models.Model):
 
 
 class SaleInfo(models.Model):
-    game = ...  # Game Model
     NA_sales = models.FloatField()
     EU_sales = models.FloatField()
     JP_sales = models.FloatField()
     other_sales = models.FloatField()
     global_sales = models.FloatField()
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    platform = models.CharField(max_length=30, blank=True)
+    publisher = models.CharField(max_length=30, blank=True)
+    developer = models.CharField(max_length=30, blank=True)
+    genre = models.CharField(max_length=30, blank=True)
+    year_of_release = models.IntegerField(blank=True)
+    esrb_rating = models.CharField(max_length=1, blank=True)
+    game_rating = models.ForeignKey(Rating, null=True, blank=True, on_delete=models.CASCADE)
+    sale_info = models.ForeignKey(SaleInfo, null=True, blank=True, on_delete=models.CASCADE)
+
