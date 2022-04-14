9abd1738e45f51e4aa38f07a2448a036c1b5dbf5
mys1erious <subtletyzeros@gmail.com>
2022-04-14 17:47:48 +0200

    Added api v1 urls to root urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from core import apiv1_urls
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(
+        route='api/v1/',
+        view=include(apiv1_urls),
+        name='api_urls'
+    ),
 
+    path('admin/', admin.site.urls),
 ]
