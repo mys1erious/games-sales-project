b0ac2205cfb63e9f6547acb3bedb5b70429e2ba1
mys1erious <subtletyzeros@gmail.com>
2022-04-16 13:05:51 +0200

    added sale API to the global api urls

--- backend/games_sales/core/apiv1_urls.py
+++ backend/games_sales/core/apiv1_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from games.api import views as game_views
+from sales.api import views as sale_views
 from accounts.api import views as account_views
 
 
@@ -17,4 +18,42 @@ urlpatterns = [
         view=game_views.GameDetailAPIView.as_view(),
         name='games'
     ),
+
+    # {% url 'api:sales' %}
+    path(
+        route='sales/',
+        view=sale_views.SaleListAPIView.as_view(),
+        name='sales'
+    ),
+    # {% url 'api:sales' sale.uuid %}
+    path(
+        route='sales/<uuid:uuid>/',
+        view=sale_views.SaleDetailAPIView.as_view(),
+        name='sales'
+    )
 ]
+
+
+
+# {
+#     "game": {
+#             "name": "name_test7",
+#             "platform": "",
+#             "publisher": "",
+#             "developer": "",
+#             "genre": "",
+#             "year_of_release": null,
+#             "esrb_rating": "",
+#             "rating": {
+#                 "critic_score": 12.0,
+#                 "critic_count": 22.0,
+#                 "user_score": 42.0,
+#                 "user_count": 120.0
+#             }
+#     },
+#     "NA_sales": 11,
+#     "EU_sales": 12,
+#     "JP_sales": 13,
+#     "other_sales": 14,
+#     "global_sales": 15
+# }
