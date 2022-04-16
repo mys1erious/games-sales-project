0696528e6f8a5740465b81091acf4c80c44f38e8
mys1erious <subtletyzeros@gmail.com>
2022-04-17 00:04:56 +0200

    Added proxy to package.json; displayed basic sales list

--- frontend/src/pages/SalesPage.js
+++ frontend/src/pages/SalesPage.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../css/SalesPage.css";
 
+
+const EMAIL = 'admin@gmail.com'
+const PASSWORD = 'admin'
 
 const SalesPage = () => {
+
+    let [sales, setSales] = useState([]);
+
+    useEffect(() => {
+        getSales();
+    }, []);
+
+    let getSales = async () => {
+        let response = await fetch('/api/v1/sales/', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Basic' + btoa(`${EMAIL}:${PASSWORD}`)
+            }
+        });
+        let data = await response.json();
+
+        setSales(data);
+    }
+
     return(
-        <div>SalesPage</div>
-    )
+        <div>
+            <h1>SalesList</h1>
+            <div>
+                {sales.map((sale, index) => (
+                    <div key={index}>{sale.game.name}</div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 
