ea61c9f52dc99a4de5f2d7d5e09b041b4c639506
mys1erious <subtletyzeros@gmail.com>
2022-04-17 00:02:33 +0200

    Created Profile model; added post_save_create_profile signal; added Profile model to admin panel

--- backend/games_sales/profiles/models.py
+++ backend/games_sales/profiles/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampedModel
+
+
+class Profile(TimeStampedModel):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(default='no bio...')
+    avatar = models.ImageField(upload_to='avatars', default='no_picture.png')
+
+    def __str__(self):
+        return f'Profile of {self.user.username}'
