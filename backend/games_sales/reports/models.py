53ced9eee294f8974db3aced6c229452762d0bef
mys1erious <subtletyzeros@gmail.com>
2022-04-17 00:03:06 +0200

    created Report model; added Report model to admin panel

--- backend/games_sales/reports/models.py
+++ backend/games_sales/reports/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampedModel
+from profiles.models import Profile
+
+
+class Report(TimeStampedModel):
+    name = models.CharField(max_length=120)
+    # image = models.ImageField ? for reports?
+    remarks = models.TextField()
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
