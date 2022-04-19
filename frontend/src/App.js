d8a966690de5a5556917aefe4885eb8449e14bba
mys1erious <subtletyzeros@gmail.com>
2022-04-19 15:07:23 +0200

    added (temporarily) getCookie method to get csrf, added SaleDetailPage to Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,10 +14,29 @@ import SalesPage from "./pages/SalesPage";
 import ReportsPage from "./pages/ReportsPage";
 import ProfilePage from "./pages/ProfilePage";
 import SignInPage from "./pages/SignInPage";
+import SaleDetailPage from "./pages/SaleDetailPage";
 
 
 
 function App() {
+
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    const csrftoken = getCookie('csrftoken');
+
     return (
         <Router>
             <Header />
@@ -25,6 +44,7 @@ function App() {
                 <Routes>
                     <Route path="/" exact element={<HomePage />} />
                     <Route path="/sales/" exact element={<SalesPage />} />
+                    <Route path="/sales/:saleUUID/" element={<SaleDetailPage />} />
                     <Route path="/reports/" exact element={<ReportsPage />} />
                     <Route path="/profile/" exact element={<ProfilePage />} />
                     <Route path="/signin/" exact element={<SignInPage />} />
