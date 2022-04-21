a47a998142f6f23126a1a8c3dd78e995bcb2a42a
mys1erious <subtletyzeros@gmail.com>
2022-04-21 17:13:41 +0200

    Created sign up serializer, sign up view, back list token view

--- backend/games_sales/accounts/api/views.py
+++ backend/games_sales/accounts/api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+from .serializers import SignUpUserSerializer
+
+
+class UserSignUpAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, format=None):
+        serializer = SignUpUserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            new_user = serializer.save()
+            if new_user:
+                return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BlackListTokenAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh_token']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
