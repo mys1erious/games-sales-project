5ff117b3fcdda701738247beb3def0c0899c3888
mys1erious <subtletyzeros@gmail.com>
2022-04-22 16:49:58 +0200

    made basic functionality to search sales by name and year of release, added basic version of searchBar to Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import '../css/Header.css';
 import {ReactComponent as Logo} from "../assets/logo.svg";
 import { useNavigate } from "react-router-dom";
+import SalesSearchBar from "./SalesSearchBar";
 
 
 const Header = () => {
 
     let navigate = useNavigate();
+    const [data, setData] = useState({ search: '' });
+
+    let renderSearchBar = () => {
+        if (window.location.href.includes('sales')) {
+           return <SalesSearchBar />
+        }
+    }
+
+    const goSearch = (e) => {
+        navigate.push({
+            pathname: '/search/',
+            search: '?search=' + data.search,
+        });
+        window.location.reload();
+    }
 
     return(
         <div className="header">
@@ -16,11 +32,20 @@ const Header = () => {
                 <span className="h_btn" onClick={() => navigate('/sales/')}>Sales</span>
                 <span className="h_btn" onClick={() => navigate('/reports/')}>Reports</span>
                 <span className="h_btn" onClick={() => navigate('/profile/')}>Profile</span>
+                <span className="h_btn">{ renderSearchBar() }</span>
             </span>
             <span className="header-panel">
-                <span className="h_btn" onClick={() => navigate('/signin/')}>Sign In</span>
-                <span className="h_btn" onClick={() => navigate('/signup/')}>Sign Up</span>
-                <span className="h_btn" onClick={() => navigate('/sign-out/')}>Sign Out</span>
+                {
+                    localStorage.getItem('access_token') !== null
+                        ?
+                        <span className="h_btn" onClick={() => navigate('/sign-out/')}>Sign Out</span>
+                        :
+                        <React.Fragment>
+                            <span className="h_btn" onClick={() => navigate('/signin/')}>Sign In</span>
+                            <span className="h_btn" onClick={() => navigate('/signup/')}>Sign Up</span>
+                        </React.Fragment>
+                }
+
             </span>
         </div>
     )
