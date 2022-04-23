f8db5d0e443ee3c2574acd99ac5e21adbca17f7c
mys1erious <subtletyzeros@gmail.com>
2022-04-23 17:26:46 +0200

    indentation fixes

--- backend/games_sales/sales/api/views.py
+++ backend/games_sales/sales/api/views.py
@@ -4,7 +4,6 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import filters
 
 from ..models import Sale
 from .serializers import SaleSerializer
