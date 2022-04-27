c7054dca1a08492bf1aa120d3a7e26a5e71f69bb
mys1erious <subtletyzeros@gmail.com>
2022-04-27 15:29:33 +0200

    Reworked Back button, added basic versions of Edit and Delete buttons

--- frontend/src/pages/SaleDetailPage.js
+++ frontend/src/pages/SaleDetailPage.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import { useNavigate, useParams} from "react-router-dom";
 import axiosInstance from "../axios";
+import {Button} from "@material-ui/core";
 
 
 const SaleDetailPage = () => {
@@ -15,22 +16,50 @@ const SaleDetailPage = () => {
 
     let getSale = async() => {
         let response = await axiosInstance.get(`/sales/${saleUUID}/`);
-        let data = await response.data;
+        let data = response.data;
         setSale(data);
     }
+
+    let handleEdit = async() => {
+        let response = await axiosInstance.put(`/sales/${saleUUID}/`, sale)
+        let data = response.data;
+        setSale(data);
+    };
+
+    let handleDelete = async() => {
+        let response = await axiosInstance.delete(`/sales/${saleUUID}/`);
+        console.log(response);
+    };
 
     // try Recursive function to get data
     return(
         <React.Fragment>
-            <button onClick={() => navigate(-1)}>Back</button>
+            <Button
+                type='submit'
+                size='medium'
+                variant='contained'
+                color='primary'
+                onClick={() => navigate(-1)}>
+                Back
+            </Button>
+            <Button
+                type='submit'
+                size='medium'
+                variant='contained'
+                color='primary'
+                onClick={handleEdit}>
+                Edit
+            </Button>
+            <Button
+                type='submit'
+                size='medium'
+                variant='contained'
+                color='primary'
+                onClick={handleDelete}>
+                Delete
+            </Button>
             <h3>Sale info:</h3>
             <pre>
-                <code>{JSON.stringify(sale, null, 4)}</code>
-
-                {/*
-                    x3 to test scrolling
-                */}
-                <code>{JSON.stringify(sale, null, 4)}</code>
                 <code>{JSON.stringify(sale, null, 4)}</code>
             </pre>
         </React.Fragment>
