b2ab7a47955903c514b50217a5acdd8d774883ff
mys1erious <subtletyzeros@gmail.com>
2022-04-27 15:30:56 +0200

    Search page for sales

--- frontend/src/pages/SalesSearchPage.js
+++ frontend/src/pages/SalesSearchPage.js
@@ -1,29 +1,48 @@
 import React, {useEffect, useState} from "react";
 import axiosInstance from "../axios";
+import {Container} from "@material-ui/core";
+import ListItem from "../components/ListItem";
 
 
-const SalesSearch = () => {
+const SalesSearchPage = () => {
     const search = 'name';
+
     const [appState, setAppState] = useState({
-        search: '',
-        sales: [],
-    });
+		search: '',
+		sales: [],
+	});
 
     useEffect(() => {
-        console.log(`sales/${window.location.search}`);
-        axiosInstance.get(`sales/${window.location.search}`).then((response) => {
-            console.log(`sales/${window.location.search}`);
-            const allSales = response.data;
-            setAppState({ sales: allSales });
-            console.log(response.data);
-        });
+        getSales();
     }, [setAppState])
+
+        const getSales = () => {
+        axiosInstance.get(`sales/${window.location.search}`)
+            .then((response) => {
+                console.log(`sales/${window.location.search}`);
+                setAppState({sales: response.data});
+            })
+            .catch((error) => {
+                if (error.response) {
+                    console.log(error.response.status);
+                }
+            });
+    }
 
     return(
         <React.Fragment>
-            hello
-		</React.Fragment>
+            <Container component="main" maxWidth="xl">
+                <h1>SalesList</h1>
+                <div>
+                    <ul>
+                        {appState.sales.map((sale, index) => (
+                        <ListItem key={index} sale={sale} />
+                    ))}
+                    </ul>
+                </div>
+            </Container>
+        </React.Fragment>
     )
 }
 
-export default SalesSearch;
+export default SalesSearchPage;
