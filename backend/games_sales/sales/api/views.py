50263bb17776c452b57820b57591c60d600397eb
mys1erious <subtletyzeros@gmail.com>
2022-04-28 15:50:38 +0200

    refactoring

--- backend/games_sales/sales/api/views.py
+++ backend/games_sales/sales/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 from rest_framework.response import Response
 from rest_framework import status
 
