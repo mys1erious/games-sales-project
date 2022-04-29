65f6b18317131082133df89d88ab81afc8320008
mys1erious <subtletyzeros@gmail.com>
2022-04-29 14:55:47 +0200

    changed avatar field to have default image

--- backend/games_sales/profiles/models.py
+++ backend/games_sales/profiles/models.py
@@ -4,10 +4,14 @@ from django.db import models
 from core.models import TimeStampedModel
 
 
+def upload_to(instance, filename):
+    return f'profiles/{filename}'
+
+
 class Profile(TimeStampedModel):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     bio = models.TextField(default='no bio...')
-    avatar = models.ImageField(upload_to='avatars', default='no_picture.png')
+    avatar = models.ImageField(upload_to=upload_to, default='profiles/no_image.png')
 
     def __str__(self):
         return f'Profile of {self.user.username}'
