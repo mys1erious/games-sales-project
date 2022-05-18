b1f2c8fa7351e423bda8baa9df229a2cd6fb5271
mys1erious <subtletyzeros@gmail.com>
2022-05-18 19:52:18 +0200

    Added functionality for deleting Sales and updating it in /sales/ list

--- frontend/src/components/sales/SaleDetailPage.js
+++ frontend/src/components/sales/SaleDetailPage.js
@@ -1,80 +1,106 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useContext} from "react";
 import { useNavigate, useParams} from "react-router-dom";
+
+import {Button, IconButton, Collapse, Alert, Container} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+
 import axiosInstance from "../../axios";
-import { Button } from '@mui/material';
+import {SalesContext} from "./SalesContext";
+
 
 const SaleDetailPage = () => {
-
     let navigate = useNavigate();
+
+    const {sales, setSales} = useContext(SalesContext);
+
     let { saleUUID } = useParams();
-    let [sale, setSale] = useState(null);
+    let [currSale, setCurrSale] = useState(null);
+    let [isDeleted, setIsDeleted] = useState(false);
+    let [isAlertOpen, setIsAlertOpen] = useState(false);
 
     useEffect(() => {
         getSale();
     }, [saleUUID]);
-    
+
     let getSale = async() => {
         let response = await axiosInstance.get(`/sales/${saleUUID}/`);
         let data = response.data;
-        setSale(data);
+        setCurrSale(data);
     }
 
     let handleEdit = async() => {
-        let response = await axiosInstance.put(`/sales/${saleUUID}/`, sale)
+        let response = await axiosInstance.put(`/sales/${saleUUID}/`, currSale)
         let data = response.data;
-        setSale(data);
+        setCurrSale(data);
     };
 
     let handleDelete = async() => {
         let response = await axiosInstance.delete(`/sales/${saleUUID}/`);
-    };
+        console.log(response.data);
 
-    // let saleFlatten = (item, dict={}) => {
-    //     console.log(item);
-    //     Object.keys(item).map((key, i) => (
-    //         (typeof(item[key]) === "object")
-    //             ? recPropsGetter(item[key], dict)
-    //             : dict[key] = item[key]
-    //     ));
-    //     return dict;
-    // };
-    //
-    // let saleDictBuilder = () => {
-    //     const dict = recPropsGetter(sale);
-    //     dict['uuid'] = saleUUID;
-    //     return dict;
-    // }
+        setIsDeleted(true);
+        setIsAlertOpen(true);
+
+        // Do i need async? just testing for large amount of data
+        setSales(sales.filter(sale => sale.uuid !== saleUUID));
+    };
 
     return(
         <React.Fragment>
-            <Button
-                type='submit'
-                size='medium'
-                variant='contained'
-                color='primary'
-                onClick={() => navigate(-1)}>
-                Back
-            </Button>
-            <Button
-                type='submit'
-                size='medium'
-                variant='contained'
-                color='primary'
-                onClick={handleEdit}>
-                Edit
-            </Button>
-            <Button
-                type='submit'
-                size='medium'
-                variant='contained'
-                color='primary'
-                onClick={handleDelete}>
-                Delete
-            </Button>
-            <h3>Sale info:</h3>
-            <pre>
-                {JSON.stringify(sale, null, 4)}
-            </pre>
+            {
+                isDeleted
+                    ?
+                    <Collapse in={isAlertOpen}>
+                        <Alert
+                          action={
+                            <IconButton
+                              aria-label="close"
+                              color="inherit"
+                              size="small"
+                              onClick={() => {
+                                setIsAlertOpen(false);
+                              }}
+                            >
+                              <CloseIcon fontSize="inherit" />
+                            </IconButton>
+                          }
+                          sx={{ mb: 2 }}>
+                          Sale has successfully been deleted !
+                        </Alert>
+                    </Collapse>
+                    : null
+            }
+            <Container component="main" maxWidth="xl">
+                <Button
+                    type='submit'
+                    size='medium'
+                    variant='contained'
+                    color='primary'
+                    onClick={() => navigate(-1)}>
+                    Back
+                </Button>
+                <Button
+                    type='submit'
+                    size='medium'
+                    variant='contained'
+                    color='primary'
+                    onClick={handleEdit}>
+                    Edit
+                </Button>
+                <Button
+                    type='submit'
+                    size='medium'
+                    variant='contained'
+                    color='primary'
+                    onClick={handleDelete}>
+                    Delete
+                </Button>
+                <h3>Sale info:</h3>
+                <pre>
+                    {JSON.stringify(currSale, null, 4)}
+                </pre>
+            </Container>
+
         </React.Fragment>
     )
 }
