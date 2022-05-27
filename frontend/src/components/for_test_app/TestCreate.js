260ee1a1ec3cecbc07758bc508cd9f318f78b26d
mys1erious <subtletyzeros@gmail.com>
2022-05-27 15:06:17 +0200

    upd

--- frontend/src/components/for_test_app/TestCreate.js
+++ frontend/src/components/for_test_app/TestCreate.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {Avatar, Button, Container, Grid, TextField, Typography} from "@mui/material";
 import {useNavigate} from "react-router-dom";
-import axiosInstance from "../Core/AxiosBase";
+import axiosInstance from "../Core/AxiosInstance";
 
 
 const TestCreate = () => {
