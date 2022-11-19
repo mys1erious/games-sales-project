19d13d58943d8c9bdffd89a6feb10dbbce9d6b68
mys1erious <subtletyzeros@gmail.com>
2022-11-19 23:53:49 +0100

    ts settings

--- nextjs-blog/pages/index.tsx
+++ nextjs-blog/pages/index.tsx
@@ -49,9 +49,9 @@ export default function Home({
     allPostsData
 }: {
     allPostsData: {
+        id: string
         date: string
         title: string
-        id: string
     }[]
 }) {
     return (
@@ -69,7 +69,13 @@ export default function Home({
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
-                    {allPostsData.map(({id, date, title}) => (
+                    {allPostsData.map(({
+                        id, date, title
+                    }: {
+                        id: string
+                        date: string
+                        title: string
+                    }) => (
                         <li className={utilStyles.listItem} key={id}>
                           <Link href={`/posts/${id}`}>{title}</Link>
                           <br />
